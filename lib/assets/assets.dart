7c64beb7c4b7a4068ceb7619a5a2a2a0386a455c
Karim Safar <kss22@mail.aub.edu>
2023-04-15 02:58:18 +0300

    fixed an error in gradle.build

---
+++ lib/assets/assets.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff188d00);
+  static const Color secondaryColor = Color(0xff284c1b);
+  static const Color accentColor = Colors.amber;
+}
+
+class WelcomeText{
+  static const String welcomeTitle = "Get the latest updates on COVID-19 vaccination efforts worldwide with our user-friendly vaccine tracking app. Stay informed about vaccination progress, availability, and distribution in your area";
+}
