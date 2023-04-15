78e28ce8d91fc2f9f74b9c78704116b158cdb257
Karim Safar <kss22@mail.aub.edu>
2023-04-15 04:27:36 +0300

    added welcome and login with backend integration

---
+++ lib/Screens/Welcome/welcome.dart
@@ -0,0 +1,75 @@
+import 'package:covid_app/assets/assets.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Welcome to Covaccine"),
+        centerTitle: true,
+        backgroundColor: AppColors.primaryColor,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Image.asset("assets/icon.jpg", height: 200, width: 200,),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              WelcomeText.welcomeTitle,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.secondaryColor),
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          SizedBox(
+            height: 50,
+            width: 300,
+            child: RaisedButton(
+              onPressed: () {},
+              child: const Text("Login", style: TextStyle(fontSize: 18.0),),
+              color: AppColors.primaryColor,
+              textColor: Colors.white,
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          SizedBox(
+            height: 50,
+            width: 300,
+            child: RaisedButton(
+              onPressed: () {},
+              child: const Text("Sign-up", style: TextStyle(fontSize: 18.0),),
+              color: AppColors.secondaryColor,
+              textColor: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
